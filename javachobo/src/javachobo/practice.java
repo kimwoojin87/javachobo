1b6abb8c97327cd9a8d968281e862a78f41cc54a
kimwoojin87 <gregorirr@gmail.com>
2019-07-15 14:32:29 +0900

    hello

---
+++ javachobo/src/javachobo/practice.java
@@ -0,0 +1,17 @@
+package javachobo;
+
+import java.util.Scanner;
+
+public class practice {
+
+	public static void main(String[] args) {
+		
+//		int a = 0;
+		char a = '0';
+        Scanner aa = new Scanner(System.in);
+//        a = aa.nextInt();
+        a = aa.next().charAt(0);
+        System.out.print(a);
+        
+	}
+}
